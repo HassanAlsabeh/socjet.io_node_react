e6af04feb9c70e9f42d626106a13fa62c667045e
Hassan Alsabeh <72017216+HassanAlsabeh@users.noreply.github.com>
2022-03-22 09:32:54 +0200

    first commit

---
+++ client/src/TypingAnimation/TypingAnimation.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./TypingAnimation.css";
+
+const TypingAnimation = () => {
+  return (
+    <div className="dotsContainer">
+      <span>typing....</span>
+    </div>
+  );
+};
+
+export default TypingAnimation;
